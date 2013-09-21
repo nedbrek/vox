8ba1f2db0f05051de73ba28b25a8124589496773
Edward Brekelbaum <nedbrek@yahoo.com>
2013-09-21 16:04:10 -0400

    Empty window

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <GL/glfw.h>
+#include <cstdio>
+
+int main(int argc, char **argv)
+{
+	// initialize GLFW
+	int rc = glfwInit();
+	if (rc != GL_TRUE)
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return 1;
+	}
+
+	// open main window
+	rc = glfwOpenWindow(1092, 614, 8, 8, 8, 8, 32, 0, GLFW_WINDOW);
+	if (rc != GL_TRUE)
+	{
+		fprintf(stderr, "Failed to open GLFW window\n");
+		glfwTerminate();
+		return 1;
+	}
+
+	bool running = true;
+	while (running)
+	{
+		// clear screen
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// swap double buffers
+		glfwSwapBuffers();
+
+		// keep running until the user hits ESC or closes the main window
+		running = !glfwGetKey(GLFW_KEY_ESC) && glfwGetWindowParam(GLFW_OPENED);
+	}
+
+	// cleanup
+	glfwTerminate();
+	return 0;
+}
+
