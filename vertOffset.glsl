0ae213e9bfc11fb5ea91786480698bf9f4968d55
Edward Brekelbaum <nedbrek@yahoo.com>
2013-10-20 08:04:07 -0400

    Fixed bug in offset W

--- vertOffset.glsl
+++ vertOffset.glsl
@@ -8,7 +8,7 @@ void main()
 	gl_TexCoord[0] = gl_Vertex;
 
 	// translate the block
-	vec4 position = gl_Vertex + uvec4(blockOffset, 1);
+	vec4 position = gl_Vertex + uvec4(blockOffset, 0);
 	
 	gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * position;
 } 
